4fe9edddd5d1505e390df184ea458d2af6843365
Miirah Furkhan <amiirahaumeeruddy@gmail.com>
2023-09-16 10:35:08 +0400

    delete client folder

--- server.js
+++ server.js
@@ -14,37 +14,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
 });
 
-/*app.get('/Register', (req, res) => {
-  res.sendFile(__dirname + '/client/Register.html');
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/client/login.html');
-});*/
-
-
-
-
-// Handle the POST request from the toDoList2 form
-app.post('/index', (req, res) => {
-
-    /*const dashboard = req.body.dashboard;
-    const reports = req.body.reports;
-    const users = req.body.users;
-    
-  
-
-  // In a real application, you should validate the credentials 
-  // and authenticate the user here.
-  // For simplicity, we'll just print the credentials for this example.
-  console.log('Received dashbord:', dashboard);
-  console.log('Received reports:', reports);
-  console.log('Received users:', users);
-  // Redirect the user to a success or failure page
-  // For simplicity, we'll always redirect to the success page.
-  res.redirect('/success.html');*/
-});
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
